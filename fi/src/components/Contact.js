cfa4c7e6addeea81849d667690eb253ddd4c9fd3
Gugan <131288000+Gugan-23@users.noreply.github.com>
2024-09-08 12:11:30 +0530

    Update Contact.js

--- fi/src/components/Contact.js
+++ fi/src/components/Contact.js
@@ -11,7 +11,7 @@ function Contact() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:5000', formData)
+    axios.post('https://personalportfolio-api.vercel.app', formData)
       .then(response => {
         
         alert('Email sent successfully!'); // Display dialog box
